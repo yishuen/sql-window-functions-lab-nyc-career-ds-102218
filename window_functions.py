c1e07bc9a7928f8e236bc7b181cfd5b8d8494f9d
yishuen <limyishuen@gmail.com>
2018-11-03 16:59:47 -0400

    finished lab

--- window_functions.py
+++ window_functions.py
@@ -1,11 +1,11 @@
 def select_distinct_total_location_sales_on_april_23():
-    return ""
+    return """SELECT DISTINCT location, sum(amount) OVER(partition by location) FROM sales WHERE date_of_sale = '2018-04-23'; """
 
 def select_distinct_location_and_avg_amount_partitioned_by_location():
-    return ""
+    return """SELECT DISTINCT location, avg(amount) OVER(partition by location) FROM sales;"""
 
 def select_distinct_date_and_total_company_wide_sales_split_by_date_ordered_by_date():
-    return ""
+    return """SELECT DISTINCT date_of_sale, sum(amount) OVER(partition by date_of_sale) FROM sales ORDER BY date_of_sale;"""
 
 def select_rank_the_sales_of_each_location_from_highest_to_lowest():
-    return ""
+    return """SELECT location, amount, rank() OVER(PARTITION BY location ORDER BY amount DESC) FROM sales;"""
